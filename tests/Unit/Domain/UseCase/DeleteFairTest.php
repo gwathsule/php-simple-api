f30be6c2d7effa366fb14e5158bc3420e3aec6b4
Rafael dos Santos Gomes <rafael-dos-santos@hotmail.com>
2022-11-29 10:33:02 -0300

    feat: cria teste unitario para delete

---
+++ tests/Unit/Domain/UseCase/DeleteFairTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Domain\UseCase;
+
+use Src\Adapter\Repository\Sqlite\FairRepositorySqlite;
+use Src\Domain\Entity\Fair\FairFactory;
+use Src\Domain\Exception\ItemNotFoundException;
+use Test\DatabaseTestCase;
+use Test\Helpers\Traits\RandomValueGenerator;
+use Src\Domain\UseCase\DeleteFair\DeleteFair as UseCase;
+
+class DeleteFairTest extends DatabaseTestCase
+{
+    use RandomValueGenerator;
+
+    private FairRepositorySqlite $repository;
+    private UseCase $useCase;
+
+    protected function setUp(): void
+    {
+        $this->repository = new FairRepositorySqlite();
+        $this->useCase = new UseCase($this->repository);
+        parent::setUp();
+    }
+
+    public function testDelete()
+    {
+        $dataDuplicated = $this->randomFairArrayData();
+        $fair = FairFactory::oneFromArray($dataDuplicated);
+        $this->repository->save($fair);
+
+        $this->useCase->execute($fair->get('id'));
+        $fairOnDatabase = $this->repository->find($fair->get('id'));
+
+        $this->assertNull($fairOnDatabase);
+    }
+
+    public function testTryDeleteNonexistent()
+    {
+        $dataDuplicated = $this->randomFairArrayData();
+        $fair = FairFactory::oneFromArray($dataDuplicated);
+        $this->expectException(ItemNotFoundException::class);
+        $this->useCase->execute($fair->get('id'));
+    }
+}
